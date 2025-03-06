0fd4c50b8ebb49d9c07570affe2cec0d897fe4f7
CameronJackman <cameron.jackman@ontariotechu.net>
2025-03-06 18:25:32 -0500

    Working Menus

---
+++ Assets/Cameron/Scripts/MenuScripts/Menus.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menus : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject pauseMenu;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            pauseMenu.SetActive(true);
+
+
+            // V freezes game so player cannot move and enemys freeze V
+            Time.timeScale = 0.0f;
+        }
+    }
+    public void startGame()
+    {
+        SceneManager.LoadScene(1);
+
+        // V sets time back to normal V
+        Time.timeScale = 1.0f;
+    }
+
+    public void quitGame()
+    {
+        Application.Quit();
+        Debug.Log("Game Closed (only works on builds)");
+
+    }
+
+    public void loadMainMenu()
+    {
+        SceneManager.LoadScene(0);
+
+    }
+
+    public void resumeGame()
+    {
+        pauseMenu.SetActive(false);
+
+        // V sets time back to normal V
+        Time.timeScale = 1.0f;
+    }
+}
